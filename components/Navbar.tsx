98e39def908b98cbf0ed40e96332436ee6f625d3
thiagolmaciel <thiagolaranjeiramaciel@hotmail.com>
2024-10-26 20:04:09 -0300

    sending

---
+++ components/Navbar.tsx
@@ -0,0 +1,92 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import { FaBars, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { ImCross } from 'react-icons/im'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => setIsOpen(!isOpen)
+
+  return (
+    <nav className='bg-transparent'>
+      <div className=' max-w-7xl mx-auto p-4 sm:px-6 lg:px-8'>
+        <div className='flex justify-between items-center w-full'>
+          <div className=''>
+            <Link href='/'>
+              <Image
+                src='/logo.png'
+                alt='logo'
+                height={200}
+                width={200}
+                className='transition-all hover:scale-105'
+              />
+            </Link>
+          </div>
+
+          <div className='hidden md:flex gap-8 items-center'>
+            <Link
+              href='/'
+              className='transition-all hover:font-bold hover:text-violet-700'
+            >
+              Home
+            </Link>
+            <Link
+              href='/'
+              className='transition-all hover:font-bold hover:text-violet-700'
+            >
+              About Me
+            </Link>
+            <Link
+              href='/'
+              className='transition-all hover:font-bold hover:text-violet-700'
+            >
+              Projects
+            </Link>
+            <Link
+              href='/contact'
+              className='transition-all hover:font-bold hover:text-violet-700'
+            >
+              Contact
+            </Link>
+            <div className='flex gap-4 items-center'>
+              <Link
+                href='https://github.com/thiagolmaciel'
+                target='_blank'
+                className='transition-all hover:text-violet-700'
+              >
+                <FaGithub size={24} className='hover:scale-110' />
+              </Link>
+              <Link
+                href='https://linkedin.com/in/thiagolmaciel'
+                target='_blank'
+                className='transition-all hover:text-violet-700'
+              >
+                <FaLinkedin size={24} className='hover:scale-110' />
+              </Link>
+            </div>
+          </div>
+          <div className='flex items-center md:hidden'>
+            <button
+              onClick={toggleMenu}
+              className='flex items-center transition-all hover:text-violet-700'
+            >
+              {isOpen ? (
+                <FaBars size={24} className='hover:scale-110' />
+              ) : (
+                <ImCross size={24} className='hover:scale-90' />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>{isOpen}</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  {href: '/', key: 'home', label: 'Home'},
+  {href: '/', key: 'about', label: 'About Me'},
+  {href: '/', key: 'projects', label: 'Projects'},
+  {href: '/contact', key: 'contact', label: 'Contact'},
+]
